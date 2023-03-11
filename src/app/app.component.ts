877b6049ccfc3265f0dac11ae2b648e8dfaa75ec
Seyfullah KARAMEŞE <seyfullahkaramese@gmail.com>
2023-03-11 14:24:52 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Call } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular_Azure';
+
+  nodejscontent:"" | undefined;
+  /**
+   *
+   */
+  constructor(private _http: HttpClient) {
+    this._http
+      .get('https://yazilimservisi-nodejs.azurewebsites.net/api/test')
+      .subscribe((res:any) => {
+        console.log(res);
+        this.nodejscontent = res.message;
+      });
+  }
 }
